8b02518e553bef373c2b6a0ad4c3c7fbbb75d3b0
itsnoturi <creightonuridil@gmail.com>
2026-04-28 15:45:43 -0500

    add terraform action

---
+++ .github/actions/terraform/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
+export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
+export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
+export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
+export STATE_KEY=${INPUT_STATE_KEY}
+export TF_STAGE=${INPUT_TF_STAGE}
+
+terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
