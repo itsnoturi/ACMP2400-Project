ba78ab9c64daa84b46419628c8b9966245662d76
itsnoturi <creightonuridil@gmail.com>
2026-04-28 16:40:46 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
-export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
-export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
-export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
-export STATE_KEY=${INPUT_STATE_KEY}
-export TF_STAGE=${INPUT_TF_STAGE}
+export ARM_CLIENT_ID="${INPUT_ARM_CLIENT_ID}"
+export ARM_CLIENT_SECRET="${INPUT_ARM_CLIENT_SECRET}"
+export ARM_SUBSCRIPTION_ID="${INPUT_ARM_SUBSCRIPTION_ID}"
+export ARM_TENANT_ID="${INPUT_ARM_TENANT_ID}"
+export STATE_KEY="${INPUT_STATE_KEY}"
+export TF_STAGE="${INPUT_TF_STAGE}"
+export DJANGO_SECRET_KEY_PROD="${INPUT_DJANGO_SECRET_KEY_PROD}"
 
-if [[ "STF_STAGE" == "stage1" ]]; then
-terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+terraform -chdir="${TF_STAGE}" init -backend-config="key=${STATE_KEY}.tfstate"
+terraform -chdir="${TF_STAGE}" plan -out="${TF_STAGE}.tfplan"
+terraform -chdir="${TF_STAGE}" apply -auto-approve "${TF_STAGE}.tfplan"
