c9d71dc898736d19ba318d48fe695bec2603fa34
itsnoturi <creightonuridil@gmail.com>
2026-04-29 13:27:18 -0500

    Update main.tf

--- t2/main.tf
+++ t2/main.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "rg-acme-final"
+    resource_group_name  = "rg-acmp-final"
     storage_account_name = "acmp2400storageaccount"
     container_name       = "big-tf-state-acmp2400"
     use_azuread_auth     = true
